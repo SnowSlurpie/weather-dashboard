b04769b87b820c5b32d4e73f145e0734b1fc6b6b
Alex Running <runniale000@gmail.com>
2022-04-08 13:07:22 -0500

    started process for fetching city coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,58 @@
-var apiKey = 'c0d05296dbfe48354a5109caac0dd8f2'
-var weatherUrl = ''
+var apiKey = 'c0d05296dbfe48354a5109caac0dd8f2';
+var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=20&lon=86&exclude=minutely,hourly,alerts&appid=" + apiKey + "&units=standard";
+var cityInput = "Irmo";
+var lat;
+var lon;
+
+
+var cities = [];
+// emojis from https://emojipedia.org/search/?q=weather
+var weatherEmojis = ["☁️","🌡️","🌫️","❄️", "🌨️", "⛅",   ]; 
+
+function loadCityData() {
+    // get city data from local storage
+    if (!localStorage.getItem("cities")) {
+        localStorage.setItem("cities", JSON.stringify(cities));
+    } else {
+        cities = JSON.parse(localStorage.getItem("cities"));
+    }
+}
+
+// return data from url
+function getInfo(url) {
+fetch(url)
+.then(function (response){
+    return response.json();
+}).then(function(data){
+    console.log(data);
+    return data;
+}).catch(function(error){
+    console.log(error.message);
+})
+}
+
+// get coorinates of a city
+function getCoords(city) {
+    var cityUrl ="http://api.openweathermap.org/geo/1.0/direct?q="+ city + "&limit=1&appid=" + apiKey;
+
+    // fetch city coordinates
+    fetch(cityUrl)
+    .then(function (response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+        if (data.length !== 0){
+            // set latitude and longitude
+            lat = data[0].lat;
+            lon = data[0].lon;
+            console.log(lat, lon);
+        } else {
+            console.log("City not found");
+        }
+    }).catch(function(error){
+        console.log(error.message);
+    })
+
+}
+
+getCoords(cityInput);
